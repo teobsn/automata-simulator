9c4382c934d9653292df77f740f21adbb56ac47b
Teodor B <teobsn@gmail.com>
2026-02-26 23:17:22 +0200

    little refactor

--- src/main.py
+++ src/main.py
@@ -15,19 +15,15 @@ import cli
 def main():
     # Parse command-line arguments
     args = cli.parse_args(sys.argv[1:])
-    
-    input_file = args.automata_data
-    input_string = args.input_string
-    output_file = args.output_file
 
     # Parse the file to get the automaton data
-    parsed_data = parser.parse(input_file)
+    parsed_data = parser.parse(args.automata_data)
 
     # Simulate the automaton with the input string
-    result = logic.simulate(parsed_data, input_string, args.write_intermediary)
+    result = logic.simulate(parsed_data, args.input_string, args.write_intermediary)
 
     # Write the result to the output file or print it
-    output.write_output(output.interpret_result(result), output_file)
+    output.write_output(output.interpret_result(result), args.output_file)
 
 if __name__ == "__main__":
     main()
