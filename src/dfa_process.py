72b92de11bab9ab9a4e3f40acb0ddd73b3bf6047
Teodor B <teobsn@gmail.com>
2026-05-14 13:35:37 +0300

    Change DFA Parser value error strings

--- src/dfa_process.py
+++ src/dfa_process.py
@@ -6,7 +6,7 @@ def section_initialize(data, section_name):
     # Check for duplicate sections
     if section_name in data:
         raise ValueError(
-            f"Parser: Duplicate section '{section_name}' found in input file."
+            f"DFA Parser: Duplicate section '{section_name}' found in input file."
         )
 
     if section_name in ["alphabet", "states", "accept_states"]:
@@ -18,7 +18,7 @@ def section_initialize(data, section_name):
     elif section_name == "settings":
         data[section_name] = []
     else:
-        raise ValueError(f"Parser: Unknown section '{section_name}' in input file.")
+        raise ValueError(f"DFA Parser: Unknown section '{section_name}' in input file.")
 
 
 def transition_add(data, source_state, input_symbol, next_state):
@@ -36,7 +36,7 @@ def transition_process_line(line):
     # Format is expected to be: current_state, input_symbol -> next_state
     for sep in ["->", ","]:
         if sep not in line:
-            raise ValueError(f"Parser: Invalid transition format in line: '{line}'")
+            raise ValueError(f"DFA Parser: Invalid transition format in line: '{line}'")
 
     # Split the line into components
     combo, next_state = line.split("->")
